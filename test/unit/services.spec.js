38db743b4cd5d162a220f5f7d56b103a3381d072
Nicholas Blair <nicholas.blair@wisc.edu>
2016-05-12 13:59:23 -0500

    WIP: treat root path fragment as proxy configuration key
    No longer require '/__/proxy' prefix.

---
+++ test/unit/services.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('proxy', function() {
+
+    it.skip('should proxy the request when configuration found', function() {
+
+    });
+    it.skip('should not proxy the request when no configuration found', function() {
+
+    });
+    it.skip('should proxy a request with query parameters', function() {
+
+    });
+    it.skip('should rewrite the proxied the request when so configured', function() {
+
+    });
+});
